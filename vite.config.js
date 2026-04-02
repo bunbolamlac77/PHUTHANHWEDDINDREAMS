8830b75761755de72686949a026dbe1d61370795
ThanhPhu <tanghuynhthanhphu@gmail.com>
2026-04-02 13:32:58 +0700

    GIAI ĐOẠN 2: XÂY DỰNG NỀN TẢNG & DESIGN SYSTEM

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,39 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['icons/*.png', 'icons/*.ico'],
+      manifest: {
+        name: 'PhuThanh Wedding Dreams',
+        short_name: 'PT Dreams',
+        description: 'Quản lý Show & Tạo Báo Giá Nhiếp Ảnh',
+        theme_color: '#121212',
+        background_color: '#121212',
+        display: 'standalone',
+        orientation: 'portrait',
+        start_url: '/',
+        scope: '/',
+        icons: [
+          { src: '/icons/icon-192.png', sizes: '192x192', type: 'image/png' },
+          { src: '/icons/icon-512.png', sizes: '512x512', type: 'image/png' },
+          { src: '/icons/icon-512.png', sizes: '512x512', type: 'image/png', purpose: 'any maskable' },
+        ],
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
+            handler: 'CacheFirst',
+            options: { cacheName: 'google-fonts', expiration: { maxEntries: 10 } },
+          },
+        ],
+      },
+    }),
+  ],
 })
